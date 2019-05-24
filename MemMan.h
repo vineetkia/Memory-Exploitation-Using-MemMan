ccafc6b64feb61efa812a333c3b0a875657be4aa
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2019-05-24 21:38:43 +0530

    Add files via upload

---
+++ MemMan.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Windows.h>
+#include <vector>
+class MemMan
+{
+public:
+	MemMan();
+	~MemMan();
+	template <class val>
+	val readMem(uintptr_t addr)
+	{
+		val x;
+		ReadProcessMemory(handle, (LPBYTE*)addr, &x, sizeof(x), NULL);
+		return x;
+	}
+	template <class val>
+	val writeMem(uintptr_t addr, val x)
+	{
+		WriteProcessMemory(handle, (LPBYTE*)addr, &x, sizeof(x), NULL);
+		return x;
+	}
+
+	uintptr_t getProcess(const wchar_t*);
+	uintptr_t getModule(uintptr_t, const wchar_t*);
+	uintptr_t getAddress(uintptr_t, std::vector<uintptr_t>);
+	
+private:
+	HANDLE handle;
+
+};
